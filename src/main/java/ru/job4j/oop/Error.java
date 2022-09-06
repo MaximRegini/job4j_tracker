842745df03ad50b0b1b092872cf5b7d3784ec6f7
MaximRegini <maxadenk@mail.ru>
2022-09-06 19:20:46 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+    public static void main(String[] args) {
+        Error test = new Error();
+        Error test1 = new Error(true, 1, "Love not found");
+        Error test2 = new Error(false, 2, "Still alive");
+        Error test3 = new Error(true, 3, "Help me");
+        test.printInfo();
+        test1.printInfo();
+        test2.printInfo();
+        test3.printInfo();
+    }
+}
